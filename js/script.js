9a781d0851c5036e6c93f16e717b648c17fff2a3
johnotu <john.otu@meltwater.org>
2016-09-25 18:29:02 +0000

    added text with terminal emulation

--- js/script.js
+++ js/script.js
@@ -1,40 +1,106 @@
-function cursorAnimation(){
-	$('#cursor').animate({
-		opacity: 0
-	}, 'fast', 'swing').animate({
-		opacity: 1
-	}, 'fast', 'swing');
+// set typing speed and wait times
+var timeInit = 4000;     // initial wait before typing first line
+var timeGap = 2000;      // wait time between each line
+var timeChar = 40;       // time until next letter
+
+var cursorChar = '&#9608;';
+
+var originId = ['line1', 'line2','line3','line4','line5','line6'];
+var originText = new Array();
+for (var i = 0; i < originId.length; i++) {
+  originText.push(document.getElementById(originId[i]).innerHTML);
 }
 
-var textLength = 0;
-var textArr = [
-	'Hello there, My name is John Otu',
-	'this is line 2',
-	'this is line 3'
-	]
+var cursorLine = document.getElementById('cursor-line');
 
-$(document).ready(function(){
-	$('.console').append('<span>johnotu:~$ </span><span id="text"></span><span id="cursor"> | </span>');
+var currentTimeout;
+var showCursor;
 
-	setInterval ('cursorAnimation()', 600);
+var typeWriter = function(index) {
+  var loc = document.getElementById(originId[index]);
+  var fullText = originText[index];
+  var letterCount = 0;
 
-	typeText();
-})
+  // this function spits out one letter per call, then calls the subsequent typeLetter()
+  var typeLetter = function() {
+    currentTimeout = setTimeout(function() {
+      loc.className = 'visible';
+      letterCount += 1;
+      var showText = fullText.substring(0, letterCount);
 
- 
+      // stops the function from self-calling when all letters are typed
+      if (letterCount === fullText.length) {
+        loc.innerHTML = ':~$ ' + showText;
+      } else {
+        loc.innerHTML = ':~$ ' + showText + '<span class="typed-cursor">' + cursorChar + '</span>';
+        typeLetter();
+      }
+    }, timeChar);
+  };
 
-function typeText(){
-	textField = $('#text');
-	text = textArr[1];
-	type();
-}
+  typeLetter();
 
-function type(){
-	textField.html(text.substr(0, textLength++));
-	if(textLength < text.length+1){
-		setTimeout('type()', 50);
-	} else {
-		textLength = 0;
-		text = '';
-	}
-}
+  // show cursor on next line
+  var totalTime = fullText.length * timeChar + timeChar;
+  showCursor = setTimeout(function() {
+    document.getElementById('cursor-line').className = 'visible';
+  }, totalTime);
+};
+
+// calculated time delays
+var delayTime = [timeInit];
+var cumulativeDelayTime = [timeInit];
+for (var i = 0; i < originId.length; i++) {
+  var elapsedTimeLine = originText[i].length * timeChar + timeGap + timeChar * 2;
+  delayTime.push(elapsedTimeLine);
+  var sum = 0;
+  for (var j = 0; j < delayTime.length; j++) {
+    sum += delayTime[j];
+  };
+  cumulativeDelayTime.push(sum);
+};
+
+// calls setTimeout for each line
+var typeLineTimeout = new Array();
+for (var i = 0; i < originId.length; i++) {
+  typeLineTimeout[i] = setTimeout((function(index) {
+    return function() {
+      cursorLine.className = 'hidden';
+      typeWriter(index);
+    }
+  })(i), cumulativeDelayTime[i]);
+
+};
+
+// stops all timeouts
+var skip = function() {
+  clearTimeout(currentTimeout);
+  clearTimeout(showCursor);
+  for (var i = 0; i < typeLineTimeout.length; i++) {
+    clearTimeout(typeLineTimeout[i]);
+  };
+};
+
+// rewrite text with value stored on page load
+
+// var rewriteText = function(index) {
+//   var loc = document.getElementById(originId[index]);
+//   loc.innerHTML = ':~$ ' + originText[index];
+//   loc.className = 'visible';
+// };
+
+var rewriteText = function(element, index, array) {
+  var loc = document.getElementById(element);
+  loc.innerHTML = ':~$ ' + originText[index];
+  loc.className = 'visible';
+};
+
+
+// trigger skip and rewrite on pressing enter or spacebar
+window.onkeydown = function(key){
+  if (key.which === 13 || key.which === 32) {
+    skip();
+    originId.forEach(rewriteText);
+    document.getElementById('cursor-line').className = 'visible';
+  }
+};
